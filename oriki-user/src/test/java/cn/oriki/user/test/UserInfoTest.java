ea912895c713b311a55f955551b62b894a39c728
王权 <n0rk1n@live.cn>
2018-09-25 22:21:25 +0800

    修改 dev-mac 环境配置

--- oriki-user/src/test/java/cn/oriki/user/test/UserInfoTest.java
+++ oriki-user/src/test/java/cn/oriki/user/test/UserInfoTest.java
@@ -1,6 +1,7 @@
 package cn.oriki.user.test;
 
 import cn.oriki.user.controller.UserInfoController;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,5 +13,10 @@ public class UserInfoTest {
 
     @Autowired
     private UserInfoController userInfoController;
+
+    @Test
+    public void test(){
+
+    }
 
 }
